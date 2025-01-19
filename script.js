2f3978e0b0baa0e9a9e4f367468c09a8d2c72607
Shakhrizoda07 <shakhrizoda752@gmail.com>
2025-01-19 00:58:46 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let user = alert('Hozir sizda radom sonlar chiqish boshlidi');
+console.log(user);
+
+
+let userNum = alert('Random orqali chiqqan son: 22');
+console.log(userNum)
+
+let userSmth = alert('Biz random sonni 2 ga kopaytirmoqchimiz va bolmoqchimiz!');
+console.log(userSmth)
+
+//let userAnything = prompt('Random chiqqan orqali son 22 edi'
+   // 'Biz uni 2 ga kopaytirib 44 sonni chiqardik,'
+   // 'Va shu sonni 2 ga bolib: 11 sonno chiqardik,'
+   // 'Va shu sonni 3 ga bolgandagi qoldiq: 1 sonimi chiqaedik');
+//alert = (userAnything);
+
+alert(Rondom orqali chiqgan son 22\n biz uni 2ga kopaytirib 44 sonini chiqardik \n biz uni 2ga bolib 11 sonini chiqardi\n va shu sonni 3 ga bolib );
